385e5ae92f8d069de9ee26e481c52f73c01bca4c
dennisja <dennisjjagwe@gmail.com>
2019-03-18 16:56:12 +0300

    add some utility functions

---
+++ lib/utils.ts
@@ -0,0 +1,112 @@
+export interface QueryStringObject {
+  key: string
+  q: string
+  format: string
+  extra: string
+  fx: string
+  mca: string
+  showlocaltime: string
+}
+
+export interface WeatherAPIData {
+  type: string
+  query: string
+  weatherDesc: Array<{ value: string }>
+  weatherIconUrl: Array<{ value: string }>
+  temp_C: string
+  temp_F: string
+  windspeedKmph: string
+  humidity: string
+  pressure: string
+  cloudcover: string
+  observation_time: string
+  localObsDateTime: string
+  utcOffset: string
+  zone: string
+}
+
+export interface RequestDetails {
+  searchedBy: string
+  value: string
+}
+
+export interface FormattedWeatherData {
+  request: RequestDetail
+  weatherDescription: string
+  weatherIconURL: string
+  temperatureDegreesCelsius: string
+  temperatureDegreesFahrenheit: string
+  windSpeedKMPH: string
+  humidity: string
+  pressure: string
+  cloudCover: string
+  observationTime: string
+  currentLocalDateTime: string
+  utcOffset: string
+  zone: string
+}
+export const makeQueryString = (
+  queryStringObject: QueryStringObject,
+): string => {
+  return Object.entries(queryStringObject).reduce(
+    (fullQueryString, currentKeyValuePair) => {
+      const [key, value]: [string, string] = currentKeyValuePair
+      const isFirstKeyValuePair: boolean = !fullQueryString
+      if (isFirstKeyValuePair) {
+        return `?${key}=${value}`
+      }
+      return `${fullQueryString}&${key}=${value}`
+    },
+    "",
+  )
+}
+
+export const replaceSpacesWithPlusSign = (str: string): string =>
+  str.replace(/\s+/g, "+")
+
+export const makeFirstLetterUpperCase = (str: string): string => {
+  const lowerCaseString = str.toLowerCase()
+  return lowerCaseString.charAt(0).toUpperCase() + lowerCaseString.slice(1)
+}
+
+export const formatLocationName = (locationName: string): string =>
+  makeFirstLetterUpperCase(replaceSpacesWithPlusSign(locationName))
+
+export const formatDataFromApi = (
+  data: WeatherAPIData,
+): FormattedWeatherData => {
+  const {
+    type: searchedBy,
+    query: value,
+    weatherDesc: [{ value: weatherDescription }],
+    weatherIconUrl: [{ value: weatherIconURL }],
+    temp_C: temperatureDegreesCelsius,
+    temp_F: temperatureDegreesFahrenheit,
+    windspeedKmph: windSpeedKMPH,
+    humidity,
+    pressure,
+    cloudcover: cloudCover,
+    observation_time: observationTime,
+    localObsDateTime: currentLocalDateTime,
+    utcOffset,
+    zone,
+  } = data
+  return {
+    request: {
+      searchedBy,
+      value,
+    },
+    weatherDescription,
+    weatherIconURL,
+    temperatureDegreesCelsius,
+    temperatureDegreesFahrenheit,
+    windSpeedKMPH,
+    humidity,
+    pressure,
+    cloudCover,
+    observationTime,
+    currentLocalDateTime,
+    utcOffset,
+    zone,
+  }
+}
