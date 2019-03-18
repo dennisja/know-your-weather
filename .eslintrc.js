d6833866ee5a27ff67113c6831fbd3cf6382d468
dennisja <dennisjjagwe@gmail.com>
2019-03-18 15:44:02 +0300

    Change prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended",
 
     // disables eslint rules that conflict with prettier
-    'prettier/@typescript-eslint',
+    "prettier/@typescript-eslint",
 
     // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
     // Should always be the last configuration in the extends array
-    'plugin:prettier/recommended',
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     // allow parsing of modern features
     ecmaVersion: 2018,
 
     // allow use of imports
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
     // override rules here
